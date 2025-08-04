0cd09f6d9f988069a3963a1175f6e3f5baff9065
MAHAN <mahan78ir@gmail.com>
2025-08-04 11:09:10 +0330

    create a list and add get all names from root

--- core/main.py
+++ core/main.py
@@ -4,6 +4,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+names_list = [
+    {"id" : 1 , "name": "mahan"},
+    {"id" : 2 , "name": "homa"},
+    {"id" : 3 , "name": "shayan"},
+    {"id" : 4 , "name": "ali"},
+    {"id" : 5 , "name": "aziz"},
+]
+
+
 @app.get('/')
 def root():
     return {"message" : "Hello, World!"}
@@ -13,3 +22,10 @@ def root():
 # if __name__ == "__main__":
 #     uvicorn.run( "main.app" , host="0.0.0.0" , port = "8000" , reload=True )
 # * for running the server : uvicorn main.app --reload --host 0.0.0.0 --port 8000
+
+
+
+
+@app.get("/names")
+def retrieve_names_list():
+    return names_list
