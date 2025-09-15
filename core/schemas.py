5ad5c3e61df3160a08292b76d1fa8acfc7abb042
MahanMa78 <mahan78ir@gmail.com>
2025-09-15 22:45:22 +0330

    add Field for validate id name

--- core/schemas.py
+++ core/schemas.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel , field_validator
+from pydantic import BaseModel , field_validator , Field , field_serializer
 #dar asl pydantic hamon naghshe serializers ro dar django ro dare 
 #ma baraye validate kardan data az tarigh api az pydantic estefadeh mikonim va #! hata baraye response 
 
+#! dar pydantic harchizi ke roye model vasle mishe (model_serializer) kol on model ro dar bar migire 
+#! va har chizi ke roye field vasle mishe (field_serializer) kol on field ro dar bar migire
+
 class BasePersonSchema(BaseModel):
-    name : str
+    name : str = Field(... , description="Enter Persons name")
 #mishe az in class ham baghie ersbari konan
+
+#*Field(...) --> mandatory yani field ejbarie
 
     @field_validator("name" ,mode="after")
     def validate_name(cls , value: str):
@@ -14,7 +19,9 @@ class BasePersonSchema(BaseModel):
             raise ValueError("Name must contain only alphabetic characters")
         return value
 
-    
+    @field_serializer("name")
+    def serialize_name(value):
+        return value.title()
 
 
 class PersonCreateSchema(BasePersonSchema):
@@ -22,7 +29,7 @@ class PersonCreateSchema(BasePersonSchema):
     
     
 class PersonResponseSchema(BasePersonSchema):
-    id : int
+    id : int =Field(...,description="Unique user identifier")
     name : str
     
     
